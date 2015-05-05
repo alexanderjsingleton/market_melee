2bb4072458c6e0b15af243d408b73a24b25f8fb5
Alex Singleton <alexanderjsingleton@gmail.com>
2015-05-05 10:47:24 -0500

    fully seeded

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development do
 end
 
 gem 'bootstrap-sass'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+#  admin = User.new(
+#    # name:     'Admin User',
+#    email:    'admin@example.com',
+#    password: 'helloworld',
+#  )
+#  admin.skip_confirmation!
+#  admin.save!
+
+#  #Create test user
+# test = User.new(
+#   email: 'test@example.com',
+#   password: 'helloworld'
+#   )
+
+# test.skip_confirmation!
+# test.save
+ 
+ 5.times do
+  registered_portfolios = Portfolio.create!(
+    # user: test,
+    name: Faker::Commerce.color
+    )
+end
+
+registered_portfolios = Portfolio.all
+
+#Create events
+10.times do
+  stocks = Stock.create!(
+    portfolio: registered_portfolios.sample,
+    # event_name: Faker::Hacker.say_something_smart
+    stock_symbol: Faker::Lorem.characters(4) 
+    )
+end
+
+
+puts "Seeds finished"
+puts "#{Portfolio.count} applications created"
+puts "#{Stock.count} events created"
