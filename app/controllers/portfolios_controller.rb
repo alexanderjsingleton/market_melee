f4d6e53c297cab15c6ade68cf9c9be08f2322ec0
Alex Singleton <alexanderjsingleton@gmail.com>
2015-05-12 12:47:11 -0500

    pulling data with yahoo finance gem; target portfolio views

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,10 +14,11 @@ class PortfoliosController < ApplicationController
   # GET /portfolios/1.json
   def show
     @portfolio = Portfolio.find(params[:id])
-    @stocks = YahooFinance.quotes([@portfolio.stocks.group_by(&:stock_symbol)], [:ask], { raw: false } )
-     
+    @stocks = YahooFinance.quotes([@portfolio.stocks.group_by(&:stock_symbol)], [:ask, :bid], { raw: false } )
+
 
     # @stocks = @portfolio.stocks.group_by(&:stock_symbol)
+    
     # @stocks = YahooFinance.quotes(["AAPL"], [:ask], { raw: false } )
     @stock = Stock.new
     @stock.portfolio_id = @portfolio.id
