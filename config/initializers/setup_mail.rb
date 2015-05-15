f5897e9ce41bbaf08a04bb733c2a8ade042854c1
Alex Singleton <alexanderjsingleton@gmail.com>
2015-05-15 12:56:04 -0500

    setup mail initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,14 +1,12 @@
-# if Rails.env.development? || if Rails.env.production?
+# if Rails.env.development? || Rails.env.production?
 if Rails.env.development?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
-    port:           '587',
-    authentication: :plain,
-    user_name:      'app36472752@heroku.com',
-    password:       'jawcrx2o',
-    domain:         'melee.herokuapp.com',
-    enable_starttls_auto: true
-  }
-end
-
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:        'smtp.sendgrid.net',
+  port:           '587',
+  authentication: :plain,
+  user_name:      ENV['SENDGRID_USERNAME'],
+  password:       ENV['SENDGRID_PASSWORD'],
+  domain:         'heroku.com',
+  enable_starttls_auto: true
+} 
